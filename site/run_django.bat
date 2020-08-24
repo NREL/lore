b3074e122311017c766aa1c79c9061cfcdd34978
Trey Winbush <trey.winbush.3@gmail.com>
2020-08-24 17:03:24 -0600

    handoff: corrected scripts to run bokeh and django

--- site/run_django.bat
+++ site/run_django.bat
@@ -1,10 +1,18 @@
 @echo off
 
-set new_key=f
-if %BOKEH_SECRET_KEY%=="" set new_key=y
-if "%1"=="create_key" set new_key=y
+set new_key="f"
 
-if "%new_key%"=="y" (
+if not defined BOKEH_SECRET_KEY ( 
+    set new_key="y"
+)
+
+if "%1"=="create_key" (
+    set new_key="y"
+)
+
+
+
+if %new_key%=="y" (
     for /f %%f in ('call bokeh secret') do (
         set BOKEH_SECRET_KEY=%%f
         echo "New Bokeh secret key created."
@@ -13,8 +21,6 @@ if "%new_key%"=="y" (
     echo "Bokeh secret key exists."
 )
 
-
-setlocal
 set address=10.10.10.10
 set port=80
 
