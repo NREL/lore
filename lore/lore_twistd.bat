7d694f5abf54b5596e185d66dbb270b569db7e72
Matthew Boyd <mr.matthew.boyd@gmail.com>
2020-09-02 12:57:22 -0600

    Setup mediator looping functionality using extensible package

---
+++ lore/lore_twistd.bat
@@ -0,0 +1,3 @@
+python C:\Users\mboyd\AppData\Local\Continuum\miniconda3\pkgs\twisted-20.3.0-py38he774522_0\Lib\site-packages\twisted\scripts\twistd.py ^
+--rundir "C:\Users\mboyd\Documents\Project Docs\Real-Time_Opt\lore\lore" ^
+--python "lore.tac"
