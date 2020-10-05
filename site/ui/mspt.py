ef77f48a7026e9ec165e2227106acdf2e29c4bf2
Matthew Boyd <mr.matthew.boyd@gmail.com>
2020-10-05 15:42:58 -0600

    Fix pysam-daotk reference and weather file path structure

--- site/ui/mspt.py
+++ site/ui/mspt.py
@@ -1,11 +1,11 @@
-import PySAM.TcsmoltenSalt as t
+import PySAM_DAOTk.TcsmoltenSalt as t
 from pathlib import Path
 import pdb
 
 def get_pysam_data():
     parent_dir = str(Path(__file__).parents[1])
-    weather_file = parent_dir+"\\data\\daggett_ca_34.865371_-116.783023_psmv3_60_tmy.csv"
-    model_name = "MSPTMerchantPlant"
+    weather_file = parent_dir+"/data/daggett_ca_34.865371_-116.783023_psmv3_60_tmy.csv"
+    model_name = "MSPTSingleOwner"
     tech_model = t.default(model_name)
     tech_model.SolarResource.solar_resource_file = weather_file
     tech_model.execute(1) # 0 = verbosity level, or you can use 1 to show verbose output
