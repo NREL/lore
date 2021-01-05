acfa7d74205b0dd77b3c77101dc2cd37eb2f31c0
zolanaj <alexander.zolan@nrel.gov>
2021-01-05 07:33:19 -0700

    add more calculated values, debug code

--- librtdispatch/dispatch_params.py
+++ librtdispatch/dispatch_params.py
@@ -9,6 +9,7 @@ import pyomo.environ as pe
 def buildParamsFromFiles(filenames):
     params_dict = {}
     for fname in filenames:
+        # print(fname)
         params_dict = buildParamsFromAMPLFile(fname, params_dict)
     params_dict = adjustParamsDict(params_dict)
     return params_dict
@@ -17,7 +18,10 @@ def buildParamsFromAMPLFile(filename, params_dict={}):
     f = open(filename, 'r')
     lines = f.readlines()
     in_param = False
+    # lineno = 1
     for line in lines:
+        # print(lineno)
+        # lineno +=1
         if line.startswith("let"):
             in_param = False
             splitline = line.split(" ")
@@ -44,16 +48,20 @@ def buildParamsFromAMPLFile(filename, params_dict={}):
 
 def adjustParamsDict(params_dict):
     ##Adjustments for john's scripts
-    params_dict["Delta"] = params_dict["dt"]
-    params_dict["Delta_e"] = params_dict["dte"]
-    params_dict["D"] = params_dict["twt"]
+    params_dict["avg_price"] = 0.0
+    sum_delta = 0.0
     params_dict["W_u_plus"] = {}
     params_dict["W_u_minus"] = {}
     params_dict["Wdotnet"] = {}
-    for t in params_dict["dt"].keys():
-        params_dict["W_u_plus"][t] = params_dict["Wdotl"] + params_dict["W_delta_plus"] * params_dict["dt"][t] / 2
-        params_dict["W_u_minus"][t] = params_dict["Wdotl"] + params_dict["W_delta_minus"] * params_dict["dt"][t] / 2
+    params_dict["F"] = {}
+    for t in params_dict["Delta"].keys():
+        params_dict["W_u_plus"][t] = params_dict["Wdotl"] + params_dict["W_delta_plus"] * params_dict["Delta"][t] / 2
+        params_dict["W_u_minus"][t] = params_dict["Wdotl"] + params_dict["W_delta_minus"] * params_dict["Delta"][t] / 2
         params_dict["Wdotnet"][t] = params_dict["Wdotu"]
+        params_dict["avg_price"] += params_dict["Delta"][t] * params_dict["P"][t]
+        sum_delta += params_dict["Delta"][t]
+        params_dict["F"][t] = int(params_dict["Qin"][t] > 0)
+    params_dict["avg_price"] /= sum_delta
     return params_dict
 
 
