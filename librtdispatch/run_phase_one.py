657f54613cdd99ec23cd9ba585678b364631a88d
zolanaj <alexander.zolan@nrel.gov>
2021-02-02 21:18:02 -0700

    fetch dispatch model updates from dao-tk-project
    Covers:
    - changes to DispatchParams class
    - new class RTDispatchOutputs, which contains all outputs from the optimization model
    - added option for grid signial
    - added option simple_receiver, which takes receiver operations as input (except for curtailing) and optimizes power cycle operations

--- librtdispatch/run_phase_one.py
+++ librtdispatch/run_phase_one.py
@@ -1,20 +1,47 @@
 """
 runs phase one of John's solution method; i.e., the linear model with a discretization of the mdotc variable.
 """
-
+from pyomo.opt import TerminationCondition
 import dispatch_model
 import dispatch_params
 import dispatch_outputs
 
-def run_phase_one(input_filenames, include, start, stop):
-    params = dispatch_params.buildParamsFromFiles(input_filenames)
-    params["start"] = start
-    params["stop"] = stop
-    params["transition"] = 0
+def run_phase_one(input_filenames, include, start, stop, printout = False):
+    params_dict = dispatch_params.buildParamsFromFiles(input_filenames)
+    params_dict["start"] = start
+    params_dict["stop"] = stop
+    params_dict["transition"] = 0
+    params = dispatch_params.getDispatchParamsFromDict(params_dict)
     rt = dispatch_model.RealTimeDispatchModel(params, include)
     rt_results = rt.solveModel()
-    outputs = dispatch_outputs.RTDispatchOutputs(rt.model)
-    outputs.print_outputs()
+    if include["simple_receiver"]:
+        outputs = dispatch_outputs.SimpleDispatchOutputs(rt.model)
+        if printout:
+            outputs.print_outputs()
+    else:
+        outputs = dispatch_outputs.RTDispatchOutputs(rt.model)
+        if printout:
+            outputs.print_outputs()
+        outputs.sendOutputsToFile()
+
+
+def run_dispatch(params, include, start, stop, transition=0):
+    params.start = start
+    params.stop = stop
+    params.transition = transition
+    rt = dispatch_model.RealTimeDispatchModel(params, include)
+    rt_results = rt.solveModel()
+    
+    if rt_results.solver.termination_condition == TerminationCondition.infeasible:
+        return False
+
+    if include["simple_receiver"]:
+        outputs = dispatch_outputs.SimpleDispatchOutputs(rt.model)
+    else:
+        outputs = dispatch_outputs.RTDispatchOutputs(rt.model)
+    #outputs.print_outputs()
+    #outputs.sendOutputsToFile()
+    return outputs
 
 if __name__ == "__main__":
     input_filenames = [
@@ -22,7 +49,8 @@ if __name__ == "__main__":
         "./input_files/init_cycle_on_full_tank.dat",
         "./input_files/time_baseline.dat"
     ]
-    include = {"pv": False, "battery": False, "persistence": False, "force_cycle": False, "op_assumptions": False}
+    include = {"pv": False, "battery": False, "persistence": False, "force_cycle": False, "op_assumptions": False,
+               "signal": True, "simple_receiver": True}
     start = 1
     stop = 68
-    run_phase_one(input_filenames, include, start, stop)
+    run_phase_one(input_filenames, include, start, stop, True)
