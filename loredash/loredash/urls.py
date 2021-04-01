db3a120358f1fd11dccafa8398e01c32dc6bb277
Quintana <dquintan@nrel.gov>
2021-04-01 14:06:18 -0600

    Reverted back to NREL/LORE code

--- loredash/loredash/urls.py
+++ loredash/loredash/urls.py
@@ -22,28 +22,6 @@ from mediation import mediator
 import multiprocessing
 import datetime
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-# ===Initialization code, put here so the Bokeh server django.setup() call doesn't execute it
-# For testing, bypassing multiprocessing:
-# parent_dir = str(Path(__file__).parents[1])
-# default_weather_file = parent_dir+"/data/daggett_ca_34.865371_-116.783023_psmv3_60_tmy.csv"
-# plant_config_path = parent_dir+"/data/plant_config.json"
-
-# mediator = mediator.Mediator(plant_config_path=plant_config_path,
-#                              override_with_weather_file_location=False,
-#                              weather_file=default_weather_file,
-#                              preprocess_pysam=True,
-#                              preprocess_pysam_on_init=True,
-#                              update_interval=datetime.timedelta(seconds=5),
-#                              simulation_timestep=datetime.timedelta(minutes=5))
-
-# result = mediator.ModelPreviousDayAndAddToDb()
-
-# result = mediator.RunOnce()
-=======
-=======
->>>>>>> 0b6710c868357517db689b029d8084353ae0fc84
 # TODO(odow): the purpose of this code is to populate the database so we have
 # things to plot. But it shouldn't go here, because this gets run on a `migrate`
 # call, and initially, we don't have a database to store the results in! It
@@ -72,10 +50,6 @@ try:
 except Exception as err:
     print("Oops! Migration failed because we don't have a database yet. Try running that command again.")
     pass
-<<<<<<< HEAD
->>>>>>> 0b6710c868357517db689b029d8084353ae0fc84
-=======
->>>>>>> 0b6710c868357517db689b029d8084353ae0fc84
 
 # This is the main production code where the mediator runs continuously
 # update_interval = 10     # seconds
@@ -94,6 +68,4 @@ urlpatterns = [
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
 
