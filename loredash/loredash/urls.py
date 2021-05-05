717f7eca92869d56ff1993e562d73796c6a4d92c
Matthew Boyd <mr.matthew.boyd@gmail.com>
2021-05-05 09:44:02 -0600

    Revert a couple changes

--- loredash/loredash/urls.py
+++ loredash/loredash/urls.py
@@ -23,20 +23,16 @@ import mediation.plant as plant_
 import multiprocessing
 import datetime
 
-# TODO(odow): the purpose of this code is to populate the database so we have
-# things to plot. But it shouldn't go here, because this gets run on a `migrate`
-# call, and initially, we don't have a database to store the results in! It
-# should probably just go somewhere so it gets run when the root site is hit.
-#
-# python manage.py migrate
-# python manage.py migrate
-def _RunOnce():
+# TODO: Ensure database migration happens before this code is run. It is currently
+# run on a 'migrate', which initially fails because no database has yet been created.
+def InitAndRunMediator():
     parent_dir = str(Path(__file__).parents[1])
     default_weather_file = parent_dir + "/data/daggett_ca_34.865371_-116.783023_psmv3_60_tmy.csv"
-    plant_design = plant_.plant_design
+    plant_config_path = parent_dir + "/data/plant_config.json"
     m = mediator.Mediator(
         params=mediator.mediator_params,
-        plant_design=plant_design,
+        plant_config_path = plant_config_path,
+        plant_design=plant_.plant_design,
         override_with_weather_file_location = False,
         weather_file = default_weather_file,
         preprocess_pysam = True,
@@ -46,10 +42,11 @@ def _RunOnce():
     result = m.ModelPreviousDayAndAddToDb()
     result = m.RunOnce()
     return
+
 # try:
-_RunOnce()
+InitAndRunMediator()
 # except Exception as err:
-#     print("Oops! Migration failed because we don't have a database yet. Try running that command again.")
+#     print("Migration failed because database has not yet been created. Try running that command again.")
 #     pass
 
 # This is the main production code where the mediator runs continuously
@@ -69,6 +66,3 @@ urlpatterns = [
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
