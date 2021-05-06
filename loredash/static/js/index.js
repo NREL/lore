80c68598a523782f3d2a380cec9188cd3d5d317e
Quintana <dquintan@nrel.gov>
2021-05-06 14:01:05 -0600

    Deleted css, img, and js files in the static folder (they are in the staticfiles subfolder now

--- loredash/static/js/index.js
+++
@@ -1,6 +0,0 @@
-// Specify which menu button gets highlighted based on path
-$(".sidebar-navigation li > a").each(function() {
-    if ((window.location.pathname == $(this).attr('href'))) {
-        $(this).parent().addClass('active');
-    }
-});
