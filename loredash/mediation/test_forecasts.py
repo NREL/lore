1a44333fcce6d0fd199b64ce35c2d9b6fb229d30
odow <o.dowson@gmail.com>
2021-05-19 13:23:01 +1200

    Switch to pytest-django

--- loredash/mediation/test_forecasts.py
+++ loredash/mediation/test_forecasts.py
@@ -1,7 +1,20 @@
 # Tests for the forecasts.py module. They can be run with:
-#  $ python manage.py test
+#  $ python -mpytest
 
-from django.test import TestCase
+import pytest
+
+# Our database gets initialized in a weird way because it only happens when you
+# run the server. As a work-around, use the current database on disk.
+# 
+# TODO(odow): remove this setup when we don't need an initialized database.
+from django.conf import settings
+@pytest.fixture(scope='session')
+def django_db_setup():
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+    return
 
 import datetime
 import pandas
@@ -11,75 +24,79 @@ import pytz
 from mediation import forecasts
 from mediation import models
 
-class ForecastTestCase(TestCase):
+# Make sure to mark any tests that need database access.
+@pytest.mark.django_db
+def test_forecaster_from_plant():
+    plant = models.PlantConfig.objects.get(pk = 1)
+    forecaster = forecasts.SolarForecast(
+        plant.latitude,
+        plant.longitude,
+        plant.timezone_string,
+        plant.elevation,
+    )
+    assert(type(forecaster.plant_location) == location.Location)
+    assert(
+        type(forecaster.forecast_uncertainty) == forecasts.ForecastUncertainty
+    )
+    return
 
-    def test_forecaster_from_plant(self):
-        plant = models.PlantConfig.objects.get(pk = 1)
-        forecaster = forecasts.SolarForecast(
-            plant.latitude,
-            plant.longitude,
-            plant.timezone_string,
-            plant.elevation,
-        )
-        assert(type(forecaster.plant_location) == location.Location)
-        assert(
-            type(forecaster.forecast_uncertainty) == forecasts.ForecastUncertainty
-        )
-        return
-    
-    def test_latestForecast(self):
-        forecaster = forecasts.SolarForecast(
-            38.2,
-            -117.4,
-            'US/Pacific',
-            100.0,
-        )
-        data = forecaster.latestForecast()
-        assert(len(data) == 48)
-        assert('clear_sky' in data.keys())
-        assert('0.5' in data.keys())
-        # Check clearsky bounds
-        assert(min(data['clear_sky']) < 100)
-        assert(max(data['clear_sky']) > 800)
-        # Check maximum solar in reasonable units, not a ratio
-        assert(max(data['1.0']) > 200)
-        return
+@pytest.mark.django_db
+def test_latestForecast():
+    forecaster = forecasts.SolarForecast(
+        38.2,
+        -117.4,
+        'US/Pacific',
+        100.0,
+    )
+    data = forecaster.latestForecast()
+    assert(len(data) == 48)
+    assert('clear_sky' in data.keys())
+    assert('0.5' in data.keys())
+    # Check clearsky bounds
+    assert(min(data['clear_sky']) < 100)
+    assert(max(data['clear_sky']) > 800)
+    # Check maximum solar in reasonable units, not a ratio
+    assert(max(data['1.0']) > 200)
+    return
 
-    def test_latestForecast_resolution(self):
-        forecaster = forecasts.SolarForecast(
-            38.2,
-            -117.4,
-            'US/Pacific',
-            100.0,
-        )
-        data = forecaster.latestForecast(resolution = '2h')
-        assert(len(data) == 24)
-        return
+@pytest.mark.django_db
+def test_latestForecast_resolution():
+    forecaster = forecasts.SolarForecast(
+        38.2,
+        -117.4,
+        'US/Pacific',
+        100.0,
+    )
+    data = forecaster.latestForecast(resolution = '2h')
+    assert(len(data) == 24)
+    return
 
-    def test_latestForecast_horizon(self):
-        forecaster = forecasts.SolarForecast(
-            38.2,
-            -117.4,
-            'US/Pacific',
-            100.0,
-        )
-        data = forecaster.latestForecast(horizon = pandas.Timedelta(hours = 24))
-        assert(len(data) == 24)
-        return
+@pytest.mark.django_db
+def test_latestForecast_horizon():
+    forecaster = forecasts.SolarForecast(
+        38.2,
+        -117.4,
+        'US/Pacific',
+        100.0,
+    )
+    data = forecaster.latestForecast(horizon = pandas.Timedelta(hours = 24))
+    assert(len(data) == 24)
+    return
 
-    def test_getForecast(self):
-        forecaster = forecasts.SolarForecast(
-            38.2,
-            -117.4,
-            'US/Pacific',
-            100.0,
-        )
-        # Choose a start time that is not current, but that NDFD will still have
-        # data for.
-        datetime_start = datetime.datetime.now(pytz.timezone('US/Pacific'))
-        data = forecaster.getForecast(
-            datetime_start = datetime_start - pandas.Timedelta(hours = 24),
-            horizon = pandas.Timedelta(hours = 24),
-            resolution = pandas.Timedelta(minutes = 1),
-        )
-        assert(len(data) == 24 * 60)
+def test_getForecast():
+    forecaster = forecasts.SolarForecast(
+        38.2,
+        -117.4,
+        'US/Pacific',
+        100.0,
+    )
+    # Choose a start time that is not current, but that NDFD will still have
+    # data for.
+    datetime_start = datetime.datetime.now(pytz.timezone('US/Pacific'))
+    data = forecaster.getForecast(
+        datetime_start = datetime_start - pandas.Timedelta(hours = 24),
+        horizon = pandas.Timedelta(hours = 24),
+        resolution = pandas.Timedelta(minutes = 1),
+    )
+    assert(len(data) == 24 * 60)
+    return
