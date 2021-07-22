6f311e5859c1e3224e8210ec3c8d376f4b47ee02
Matthew Boyd <mr.matthew.boyd@gmail.com>
2021-07-22 13:48:52 -0600

    Stop serving unused bokeh plots

--- loredash/run_bokeh_server.ps1
+++ loredash/run_bokeh_server.ps1
@@ -16,16 +16,25 @@ conda activate loredash
 
 python -m bokeh serve `
     ./ui/BokehApps/dashboard_plot `
-    ./ui/BokehApps/historical_dashboard_plot `
-    ./ui/BokehApps/historical_solar_forecast `
-    ./ui/BokehApps/market_plot `
     ./ui/BokehApps/solar_plot `
-    ./ui/BokehApps/estimates_table.py `
-    ./ui/BokehApps/probability_table.py `
-    ./ui/BokehApps/sliders.py `
-    ./ui/BokehApps/__init__.py `
     --allow-websocket-origin 127.0.0.1:8000 `
 	--address 127.0.0.1 `
 	--port 5006 `
 	--check-unused-sessions 17000 `
     --unused-session-lifetime 600000
+	
+#python -m bokeh serve `
+#    ./ui/BokehApps/dashboard_plot `
+#    ./ui/BokehApps/historical_dashboard_plot `
+#    ./ui/BokehApps/historical_solar_forecast `
+#    ./ui/BokehApps/market_plot `
+#    ./ui/BokehApps/solar_plot `
+#    ./ui/BokehApps/estimates_table.py `
+#    ./ui/BokehApps/probability_table.py `
+#    ./ui/BokehApps/sliders.py `
+#    ./ui/BokehApps/__init__.py `
+#    --allow-websocket-origin 127.0.0.1:8000 `
+#	--address 127.0.0.1 `
+#	--port 5006 `
+#	--check-unused-sessions 17000 `
+#    --unused-session-lifetime 600000
