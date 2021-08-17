3cbae65e56ec481c6de386ec5fbc25234e5eb89c
odow <o.dowson@gmail.com>
2021-08-17 15:43:51 +1200

    Tidy test case

--- loredash/tests/test_single_run_cases.py
+++ loredash/tests/test_single_run_cases.py
@@ -1,5 +1,5 @@
 # Test case study.
-# To run this file: python -m pytest mediation/test_mediator.py
+# To run this file: python -m pytest mediation/test_single_run_casescpy
 import pytest
 import pathlib
 import datetime
@@ -13,29 +13,21 @@ from mediation.models import TechData
 
 @pytest.mark.django_db
 def test_single_run_case():
-    plant_design_path = PARENT_DIR + "/tests/inputs/plant_design_test.json"
-    mediator_params_path = PARENT_DIR + "/tests/inputs/mediator_settings_test.json"
-    dispatch_params_path = PARENT_DIR + "/tests/inputs/dispatch_settings_test.json"
-    weather_file_path = PARENT_DIR + "/data/daggett_ca_34.865371_-116.783023_psmv3_60_tmy.csv"
     m = mediator.Mediator(
-        params_path = mediator_params_path,
-        plant_design_path=plant_design_path,
-        weather_file = weather_file_path,
-        dispatch_params_path=dispatch_params_path,
-        update_interval = datetime.timedelta(seconds = 5),
+        params_path=PARENT_DIR + "/tests/inputs/mediator_settings_test.json",
+        plant_design_path=PARENT_DIR + "/tests/inputs/plant_design_test.json",
+        weather_file=PARENT_DIR + "/data/daggett_ca_34.865371_-116.783023_psmv3_60_tmy.csv",
+        dispatch_params_path=PARENT_DIR + "/tests/inputs/dispatch_settings_test.json",
+        update_interval=datetime.timedelta(seconds=5),
     )
-    tzinfo = pytz.UTC
-    datetime_start = datetime.datetime(2021, 6, 1, 0, 0, 0, tzinfo=tzinfo)
-    datetime_end = datetime.datetime(2021, 6, 3, 0, 0, 0, tzinfo=tzinfo)
-    m.run_once(datetime_start=datetime_start,
-                         datetime_end=datetime_end)
+    m.run_once(
+        datetime_start=datetime.datetime(2021, 6, 1, 0, 0, 0, tzinfo=pytz.UTC),
+        datetime_end=datetime.datetime(2021, 6, 3, 0, 0, 0, tzinfo=pytz.UTC),
+    )
     queryset = TechData.objects.values_list('W_grid_no_derate', 'E_tes_charged')
     df = pd.DataFrame.from_records(queryset)
-    sum_w_grid_no_derate = round(df[0].values.sum() / 12000, 0) # Net output energy [MWh-e]
-    avg_tes = round(df[1].values.sum() / (1000*len(df[1].values)), 0) # Avg. TES SOC [MWh-t]
-    assert(sum_w_grid_no_derate == 1113)
-    assert(avg_tes == 1266)
-
-
-if __name__ == "__main__":
-    test_single_run_case()
+    # Test the net output energy [MWh-e]
+    assert(round(df[0].values.sum() / 12000, 0)  == 1113)
+    # test the avg. TES SOC [MWh-t]
+    assert(round(df[1].values.sum() / (1000*len(df[1].values)), 0) == 1266)
+    return
