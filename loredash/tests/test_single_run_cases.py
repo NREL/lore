7f95c1b68c486c24f2a74c18f7eb063b05194739
zolanaj <alexander.zolan@nrel.gov>
2021-08-31 13:26:28 -0600

    add comparison plot test

--- loredash/tests/test_single_run_cases.py
+++ loredash/tests/test_single_run_cases.py
@@ -31,3 +31,26 @@ def test_single_run_case():
     # test the avg. TES SOC [MWh-t]
     assert(round(df[1].values.sum() / (1000*len(df[1].values)), 0) == 1266)
     return
+
+@pytest.mark.django_db
+def test_comparison_plot():
+    import os
+    try:
+        os.remove("./dispatch_plots.png")
+    except FileNotFoundError:
+        pass
+    m = mediator.Mediator(
+        params_path=PARENT_DIR + "/tests/inputs/mediator_settings_plots.json",
+        plant_design_path=PARENT_DIR + "/tests/inputs/plant_design_test.json",
+        weather_file=PARENT_DIR + "/data/daggett_ca_34.865371_-116.783023_psmv3_60_tmy.csv",
+        dispatch_params_path=PARENT_DIR + "/tests/inputs/dispatch_settings_test.json",
+        update_interval=datetime.timedelta(seconds=5),
+    )
+    m.run_once(
+        datetime_start=datetime.datetime(2021, 6, 1, 0, 0, 0, tzinfo=pytz.UTC),
+        datetime_end=datetime.datetime(2021, 6, 3, 0, 0, 0, tzinfo=pytz.UTC),
+    )
+    # Test the existence of the plots file
+    assert(os.path.exists("./dispatch_plots.png"))
+    os.remove("./dispatch_plots.png")
+    return
